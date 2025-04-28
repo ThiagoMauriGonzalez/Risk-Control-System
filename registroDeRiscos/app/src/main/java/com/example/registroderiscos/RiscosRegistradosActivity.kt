41b90b32e86510b0ad5693ef4dd05cdaf1f21d5c
Thiago Mauri Gonzalez <thimauri06@gmail.com>
2025-04-28 20:36:48 -0300

    interligando tela

--- registroDeRiscos/app/src/main/java/com/example/registroderiscos/RiscosRegistradosActivity.kt
+++ registroDeRiscos/app/src/main/java/com/example/registroderiscos/RiscosRegistradosActivity.kt
@@ -1,6 +1,8 @@
 package com.example.registroderiscos
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,14 @@ class RiscosRegistradosActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnNovoRisco = findViewById<Button>(R.id.btnNovoRisco)
+
+        btnNovoRisco.setOnClickListener {
+
+            startActivity(Intent(this, RegistroDeRiscos::class.java))
+            finish()
         }
     }
 }
